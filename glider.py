c3d585d3b1a2702f1d9e0d2c60da908b7d8ed9f6
Lucas Jansen Klomp <l.f.jansenklomp@utwente.nl>
2022-01-26 16:16:41 +0100

    added data cleaning and contraction plot

--- glider.py
+++ glider.py
@@ -1,23 +1,43 @@
 import math
+from msilib.schema import ListView
 import numpy as np
 import pickle
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-#data = pickle.load(open('data.p', 'wb'))
+data = pickle.load(open('data_points-paddle4-zoom75.p', 'rb'))
 
-dat = [[[np.random.uniform(0.0, 1.0), np.random.uniform(0.0, 1.0)] for i in range(4)]]
+fnd = False
+ind = 0
 
-for k in range(100):
-    newp = [[0,0],[0,0],[0,0],[0,0]]
-    for j in range(4):
-        newp[j][0] = dat[-1][j][0] + np.random.uniform(-0.05, 0.05)
-        newp[j][1] = dat[-1][j][1] + np.random.uniform(-0.05, 0.05)  #+ [[np.random.uniform(-0.1, 0.1), np.random.uniform(-0.1, 0.1)] for i in range(2)]
-    dat.append(newp)
+print('datapunt: ', data[40])
+t = list(range(1,len(data)+1))
 
-t = list(range(1,len(dat)+1))
 
-data = dat
+# clean data
+for k in range(len(data)):
+    if data[k][0] == [0,0]:
+        if k+1 != len(data) and data[k+1][0] != [0,0]:
+            data[k] = (np.add(data[k-1],data[k+1]))/2
+        else:
+            data[k] = data[k-1]
+
+    rem = False
+    if k > 0:
+        for j in range(4):
+            print(np.linalg.norm(np.subtract(data[k][j], data[k-1][j])))
+            if np.linalg.norm(np.subtract(data[k][j], data[k-1][j])) > 200:
+                rem = True
+    
+    if rem:
+        data[k] = data[k-1]
+
+ma_data = np.copy(data)
+for k in range(len(data)):
+    ma_data[k] = np.sum(data[k:k+30],axis=0)/30
+
+data = ma_data
+
 def find_intersection(x1, y1, x2, y2, x3, y3, x4, y4):
     x_c = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / (
                 (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4))
@@ -35,8 +55,17 @@ vf = []
 vb = []
 hl = []
 hr = []
+alphavec = []
+contrvec = []
+
+ind = 0
+LV = 7
+LH = 18
+K = LH/LV
+
 
 for vertical_back, vertical_front, horizontal_left, horizontal_right in data:
+
     x1, y1 = vertical_back
     x2, y2 = vertical_front
     x3, y3 = horizontal_left
@@ -59,14 +88,23 @@ for vertical_back, vertical_front, horizontal_left, horizontal_right in data:
     horizontal_length = math.sqrt((x3-x4)**2 + (y3-y4)**2)
     hlength.append(horizontal_length)
 
+    ex_horiz_length = K*vertical_length
+    horiz_contr = horizontal_length/ex_horiz_length
+    contrvec.append(horiz_contr)
     angle = math.atan((y1-y2)/(x1-x2))  # this is the angle with respect to the horizon (positive x-axis)
 
+    m1 = (y2-y1)/(x2-x1)
+    m2 = (y4-y3)/(x4-x3)
+    alpha = np.arctan(abs((m2-m1)/(1+m1*m2)))
+    alphavec.append(alpha)
     height = vertical_length/vertical_line_length
     directions.append(height)
     #relative_height.append(relative_height)
 
     left_wing_length = math.sqrt((x3-x_center)**2 + (y3-y_center)**2)
     right_wing_length = math.sqrt((x4-x_center)**2 + (y4-y_center)**2)
+
+    ind += 1
 
 # collect data
 cpoints = np.array(center_points)
@@ -104,6 +142,9 @@ plt.xlabel('x')
 plt.ylabel('y')
 plt.show()
 
+plt.plot(xc,yc)
+plt.show()
+
 # x center trajectory
 plt.plot(t,xc)
 plt.title('Center of glider trajectory')
@@ -118,9 +159,16 @@ plt.xlabel('t')
 plt.ylabel('y center')
 plt.show()
 
-# plt.plot(t,vlength)
-# plt.show()
+# angle on cross
+plt.plot(t,alphavec)
+plt.title('Center of glider trajectory')
+plt.xlabel('t')
+plt.ylabel('alpha')
+plt.show()
 
-# plt.plot(t,hlength)
-# plt.show()
-
+# contraction
+plt.plot(t,contrvec)
+plt.title('Contraction of glider')
+plt.xlabel('t')
+plt.ylabel('Contraction')
+plt.show()
